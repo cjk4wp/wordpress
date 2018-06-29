9930931040d3bb3312e0a7954dfbd4fd8931032d
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-06-29 12:56:55 +0900

    new patch

--- src/wp-login.php
+++ src/wp-login.php
@@ -11,16 +11,7 @@
 /** Make sure that the WordPress bootstrap has run before continuing. */
 require( dirname( __FILE__ ) . '/wp-load.php' );
 
-// Redirect to https login if forced to use SSL
-if ( force_ssl_admin() && ! is_ssl() ) {
-	if ( 0 === strpos( $_SERVER['REQUEST_URI'], 'http' ) ) {
-		wp_safe_redirect( set_url_scheme( $_SERVER['REQUEST_URI'], 'https' ) );
-		exit();
-	} else {
-		wp_safe_redirect( 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] );
-		exit();
-	}
-}
+echo 'hello';
 
 /**
  * Output the login page header.
